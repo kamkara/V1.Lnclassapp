34228a03822e401f8bd46d94ae248d158490f827
kamkara <lnclass.kamkara@gmail.com>
2021-11-12 11:09:44 +0000

    app: sign in fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, authentication_keys:[]
+         :recoverable, :rememberable, :validatable, :trackable
 
   has_many :materials
   has_many :levels
