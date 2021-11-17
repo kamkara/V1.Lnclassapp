246f88d5adcfe6b311436a611da5da08aa8732a7
kamkara <lnclass.kamkara@gmail.com>
2021-11-17 13:12:36 +0000

    add prof partner sign up and student

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,12 @@ class ApplicationController < ActionController::Base
     #login params
     before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :find_levels
-    before_action :find_school_names
+    before_action :find_schools
 
   protected
     # If you have extra params to permit, append them to the sanitizer.
     def configure_permitted_parameters
-    sign_up_params          = [ :first_name, :last_name, :full_name, :matricule, :level_id, :material_id, :school_name, :contact, :city, :role,
+    sign_up_params          = [ :first_name, :last_name, :full_name, :matricule, :level_id, :material_id, :school_id, :contact, :city, :role,
                               :email, :password, :slug, :gender]
     update_sign_up_params  = [ :school_name, :status, :avatar_profil, :status_payment]
      
@@ -21,10 +21,11 @@ class ApplicationController < ActionController::Base
     end 
 
     private
-    def find_levels
-      @levels = Level.all
-    end
-    def find_school_names
-      @school_names = School.all
-    end
+      def find_levels
+        @levels = Level.all
+      end
+      
+      def find_schools
+        @schools = School.all
+      end
 end
