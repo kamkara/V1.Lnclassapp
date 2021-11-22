6eb1fa4e1d4fa794e79987c0fda507d7700da8af
kamkara <lnclass.kamkara@gmail.com>
2021-11-22 16:03:21 +0000

    add

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -13,6 +13,7 @@ class ClassroomsController < ApplicationController
   # GET /classrooms/1 or /classrooms/1.json
   def show
     if current_user.role == "Student"
+      @classroom = Classroom.find(params[:id])
       render :teacher_view_classroom
     end
   end
