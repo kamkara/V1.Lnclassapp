ff29e155564b1890fe6b339d6697f39dce958c5f
kamkara <lnclass.kamkara@gmail.com>
2021-11-23 10:56:32 +0000

    app/helpers/classrooms_helper: add classroom method

--- app/helpers/classrooms_helper.rb
+++ app/helpers/classrooms_helper.rb
@@ -1,5 +1,4 @@
 module ClassroomsHelper
-
     #student_results_count
     def student_results_count(exercice)
         result_count = exercice.results.count
@@ -7,6 +6,22 @@ module ClassroomsHelper
         return "#{stub} l'exercice."
     end
 
+    def unattempted_exercices(classroom, user)
+        group.exercices.ready.filter do |q| 
+            !q.results.find_by(user: user)
+        end
+    end
 
+    def finished_exercices(classroom, user)
+        group.exercices.ready.filter do |q| 
+            q.results.find_by(user: user)
+        end
+    end
     
+    def exercices_due(classroom)
+     exercice_readed = classroom.exercices.ready.count 
+     classroom_result = classroom.results.where(user: current_user).count
+     exercice_readed - classroom_result
+     classroom.exercices.ready.count - classroom.results.where(user: current_user).count
+    end
 end
