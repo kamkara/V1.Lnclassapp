a3f084d2911324ff2d8d3c70a7957c97e82f3b5c
kamkara <lnclass.kamkara@gmail.com>
2021-11-23 14:27:01 +0000

    add result

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,21 +1,21 @@
-class ResultController < ApplicationController
+class ResultsController < ApplicationController
   before_action :authenticate_user!
 
   def new
     #authorize Result, :new?
-    @result = Exercice.find(params[:exercice_id]).build_result()
+    @result = Exercice.friendly.find(params[:exercice_id]).build_result()
   end
     
   def create
     #authorize Result, :create?
-    @result = Result.new(result_params.merge({user: current_user}))
+    @result = Result.new(results_params.merge({user: current_user}))
     redirect_to classroom_path(@result.exrcice.classroom) and return if @result.save
     render :new
   end
 
   private
 
-  def result_params
+  def results_params
     params.require(:result).permit( :exercice_id, answered_questions_attributes: [ :answer_id, :question_id])
   end
 end
