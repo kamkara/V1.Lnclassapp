aa87b2cf1837ea2be8ab69c715e9ee09cc7f7467
kamkara <lnclass.kamara+ln@gmail.com>
2021-12-21 17:07:53 +0000

    remove database host

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, authentication_keys:[:logged]
+         :recoverable, :rememberable, :validatable, :timeoutable, :trackable, authentication_keys:[:logged]
 
   has_many :questions
   has_many :materials
--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ module V1Lnclassapp
     config.hosts << "www.lnclass.com"
     config.hosts << "lnclass.com"
     config.hosts << "www.main-bvxea6i-4aqgsifxsdsmw.uk-1.platformsh.site"
-    config.hosts <<  "7msgbfpwdqtlhlklrdffmmddui.dbpostgres.service._.uk-1.platformsh.site"
 
     # Configuration for the application, engines, and railties goes here.
     #
